10fbcb4213a8f4df261ebcd3f913d04f0f81dd76
ryan <ryanwaszak@gmail.com>
2020-04-06 20:23:37 -0500

    add axios get request to index page on localhost:3000 from localhost:8080

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <h2> {{name}}</h2>
+    <div v-bind:key="product.id" v-for="product in products">
+      <p>{{ product.name }}</p>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
+import axios from "axios";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      name: "Ryan",
+      products:[]
+    };
+  },
+  created: function() {
+    console.log("I was auto-created");
+    axios.get("/api/products").then(response => {
+      console.log("I'm inside the callback");
+      console.log(response.data);
+      this.products = response.data;
+    })
+  },
+  methods: {}
+};
 </script>
