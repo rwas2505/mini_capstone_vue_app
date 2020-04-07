6270b4fc2e838fd0e983a33589376abf2dda6801
ryan <ryanwaszak@gmail.com>
2020-04-07 09:54:00 -0500

    add dynamic user input to Home.vue so user can create new product

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,11 @@
   <div class="home">
     <h1>{{ message }}</h1>
     <h2> {{name}}</h2>
+    <p>Name: <input type="text" v-model="newProductName"></p>
+    <p>Price: <input type="text" v-model="newProductPrice"></p>
+    <p>Description: <input type="text" v-model="newProductDescription"></p>
+    <p>Image URL: <input type="text" v-model="newProductImageUrl"></p>
+    <button v-on:click="addProduct()">Create</button>
     <div v-bind:key="product.id" v-for="product in products">
       <p>{{ product.name }}</p>
     </div>
@@ -18,7 +23,11 @@ export default {
     return {
       message: "Welcome to Vue.js!",
       name: "Ryan",
-      products:[]
+      products:[],
+      newProductName: "",
+      newProductPrice: null,
+      newProductDescription: "",
+      newProductImageUrl: ""
     };
   },
   created: function() {
@@ -27,8 +36,29 @@ export default {
       console.log("I'm inside the callback");
       console.log(response.data);
       this.products = response.data;
-    })
+    });
   },
-  methods: {}
+  methods: {
+    addProduct: function() {
+
+      console.log("This should add a product");
+      
+      var params = {
+        name: this.newProductName,
+        price: this.newProductPrice,
+        description: this.newProductDescription,
+        image_url: this.newProductImageUrl
+      };
+
+      axios.post("/api/products", params).then(response => {
+        console.log(response.data);
+        this.products.push(response.data);
+        this.newProductName = "";
+        this.newProductPrice = "";
+        this.newProductDescription = "";
+        this.newProductImageUrl = "";
+      });
+    }
+  }
 };
 </script>
